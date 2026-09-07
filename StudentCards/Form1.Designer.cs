e0722d8a4bbcb42a1447b237e8c534844817fc9d
Mr.Beast <gxcorn@mail.ru>
2026-09-07 23:28:38 +0500

    Добавьте файлы проекта.

---
+++ StudentCards/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace StudentCards
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtExcel = new System.Windows.Forms.TextBox();
+            this.numStart = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numEnd = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numCardStart = new System.Windows.Forms.NumericUpDown();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.txtTemplate = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numEnd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCardStart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtExcel
+            // 
+            this.txtExcel.Location = new System.Drawing.Point(18, 18);
+            this.txtExcel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtExcel.Name = "txtExcel";
+            this.txtExcel.Size = new System.Drawing.Size(246, 31);
+            this.txtExcel.TabIndex = 0;
+            this.txtExcel.Text = "Сюда Путь К Excel";
+            this.txtExcel.TextChanged += new System.EventHandler(this.txtExcel_TextChanged);
+            // 
+            // numStart
+            // 
+            this.numStart.Location = new System.Drawing.Point(13, 125);
+            this.numStart.Name = "numStart";
+            this.numStart.Size = new System.Drawing.Size(120, 31);
+            this.numStart.TabIndex = 2;
+            this.numStart.ValueChanged += new System.EventHandler(this.numStart_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(494, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "с какой строки Excel начинать печать (минимум 1)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(394, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "по какую строку печатать (например, 30)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // numEnd
+            // 
+            this.numEnd.Location = new System.Drawing.Point(13, 162);
+            this.numEnd.Name = "numEnd";
+            this.numEnd.Size = new System.Drawing.Size(120, 31);
+            this.numEnd.TabIndex = 4;
+            this.numEnd.ValueChanged += new System.EventHandler(this.numEnd_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(494, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "номер самого студенческого билета (например, 256)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // numCardStart
+            // 
+            this.numCardStart.Location = new System.Drawing.Point(12, 199);
+            this.numCardStart.Name = "numCardStart";
+            this.numCardStart.Size = new System.Drawing.Size(120, 31);
+            this.numCardStart.TabIndex = 6;
+            this.numCardStart.ValueChanged += new System.EventHandler(this.numCardStart_ValueChanged);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Font = new System.Drawing.Font("Unispace", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrint.Location = new System.Drawing.Point(18, 256);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(188, 54);
+            this.btnPrint.TabIndex = 8;
+            this.btnPrint.Text = "{ПЕЧАТЬ}";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click_1);
+            // 
+            // txtTemplate
+            // 
+            this.txtTemplate.Location = new System.Drawing.Point(18, 74);
+            this.txtTemplate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTemplate.Name = "txtTemplate";
+            this.txtTemplate.Size = new System.Drawing.Size(246, 31);
+            this.txtTemplate.TabIndex = 1;
+            this.txtTemplate.Text = "Сюда Путь К Шаблону";
+            this.txtTemplate.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 485);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numCardStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numEnd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numStart);
+            this.Controls.Add(this.txtTemplate);
+            this.Controls.Add(this.txtExcel);
+            this.Font = new System.Drawing.Font("Unispace", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "CardMakerV1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numEnd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCardStart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtExcel;
+        private System.Windows.Forms.NumericUpDown numStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numEnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numCardStart;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.TextBox txtTemplate;
+    }
+}
+
---
+++ StudentCards/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+
+namespace StudentCards
+{
+    public partial class Form1 : Form
+    {
+        public class Student
+        {
+            public string Iin { get; set; }
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public string DateOfBirth { get; set; }
+            public string Group { get; set; }
+            public string Speciality { get; set; }
+            public Image Photo { get; set; }
+        }
+
+        private List<Student> studentsToPrint = new List<Student>();
+        private Image templateImage;
+        private int currentPrintIndex = 0;
+        private int currentCardNumber = 1;
+        private string logFilePath = "print_log.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                ExcelPackage.License.SetNonCommercialPersonal("StudentCardsApp");
+            }
+            catch
+            {
+#pragma warning disable CS0618
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+#pragma warning restore CS0618
+            }
+        }
+
+        private void Log(string message)
+        {
+            string line = $"[{DateTime.Now:HH:mm:ss}] {message}";
+            try
+            {
+                File.AppendAllText(logFilePath, line + Environment.NewLine);
+            }
+            catch { }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            Log("--- СТАРТ НОВОЙ ЗАДАЧИ ПЕЧАТИ ---");
+            try
+            {
+                if (!File.Exists(txtExcel.Text))
+                {
+                    MessageBox.Show("Файл Excel не найден по указанному пути!");
+                    return;
+                }
+
+                if (!File.Exists(txtTemplate.Text))
+                {
+                    MessageBox.Show("Файл шаблона не найден по указанному пути!");
+                    return;
+                }
+
+                LoadExcelData();
+
+                templateImage = Image.FromFile(txtTemplate.Text);
+                Log($"Шаблон загружен успешно. Разрешение: {templateImage.Width}x{templateImage.Height}");
+
+                currentCardNumber = (int)numCardStart.Value;
+                currentPrintIndex = 0;
+
+                PrintDocument pd = new PrintDocument();
+                pd.DefaultPageSettings.Landscape = true;
+
+                PrintDialog pdi = new PrintDialog();
+                pdi.Document = pd;
+
+                if (pdi.ShowDialog() == DialogResult.OK)
+                {
+                    Log($"Выбран принтер: {pd.PrinterSettings.PrinterName}. Начинается отправка...");
+                    pd.PrintPage += Pd_PrintPage;
+                    pd.Print();
+                    Log("Все страницы успешно отправлены в очередь печати.");
+                    MessageBox.Show("Печать завершена успешно!");
+                }
+                else
+                {
+                    Log("Печать отменена пользователем в диалоговом окне.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"КРИТИЧЕСКАЯ ОШИБКА: {ex.Message}\n{ex.StackTrace}");
+                MessageBox.Show($"Ошибка: {ex.Message}\nПодробности записаны в {logFilePath}");
+            }
+            finally
+            {
+                if (templateImage != null)
+                {
+                    templateImage.Dispose();
+                    templateImage = null;
+                }
+
+                foreach (var s in studentsToPrint)
+                {
+                    if (s.Photo != null)
+                    {
+                        s.Photo.Dispose();
+                        s.Photo = null;
+                    }
+                }
+            }
+        }
+
+        private void LoadExcelData()
+        {
+            studentsToPrint.Clear();
+            Log($"Открытие файла: {txtExcel.Text}");
+
+            using (var package = new ExcelPackage(new FileInfo(txtExcel.Text)))
+            {
+                if (package.Workbook.Worksheets.Count == 0)
+                    throw new Exception("В файле Excel нет листов!");
+
+                var ws = package.Workbook.Worksheets[0];
+                int totalRows = ws.Dimension?.Rows ?? 0;
+                Log($"Лист '{ws.Name}' загружен. Всего строк: {totalRows}");
+
+                var pictures = ws.Drawings.OfType<ExcelPicture>().ToList();
+                Log($"Найдено графических объектов (фото) в таблице: {pictures.Count}");
+
+                int startRow = (int)numStart.Value + 1; // 1-я строка заголовки
+                int endRow = (int)numEnd.Value + 1;
+                if (endRow > totalRows) endRow = totalRows;
+
+                Log($"Диапазон чтения строк Excel: с {startRow} по {endRow}");
+
+                for (int row = startRow; row <= endRow; row++)
+                {
+                    string iin = ws.Cells[row, 1].Text.Trim();
+                    if (string.IsNullOrEmpty(iin))
+                    {
+                        Log($"Строка {row}: ИИН пустой, пропуск строки.");
+                        continue;
+                    }
+
+                    Image studentPhoto = null;
+
+                    var pic = pictures.FirstOrDefault(p => p.From.Row == row - 1);
+                    if (pic != null)
+                    {
+                        try
+                        {
+                            using (var ms = new MemoryStream(pic.Image.ImageBytes))
+                            {
+                                studentPhoto = Image.FromStream(ms);
+                            }
+                            Log($"Строка {row} (ИИН: {iin}): фото успешно извлечено.");
+                        }
+                        catch (Exception pEx)
+                        {
+                            Log($"Строка {row} (ИИН: {iin}): сбой чтения картинки -> {pEx.Message}");
+                        }
+                    }
+                    else
+                    {
+                        Log($"ВНИМАНИЕ: Строка {row} (ИИН: {iin}): фото не найдено в ячейке!");
+                    }
+
+                    studentsToPrint.Add(new Student
+                    {
+                        Iin = iin,
+                        LastName = ws.Cells[row, 2].Text.Trim(),
+                        FirstName = ws.Cells[row, 3].Text.Trim(),
+                        DateOfBirth = ws.Cells[row, 4].Text.Trim(),
+                        Group = ws.Cells[row, 5].Text.Trim(),
+                        Speciality = ws.Cells[row, 6].Text.Trim(),
+                        Photo = studentPhoto
+                    });
+                }
+            }
+
+            Log($"Готово к печати студентов: {studentsToPrint.Count}");
+            if (studentsToPrint.Count == 0)
+                throw new Exception("Не удалось найти ни одного студента в выбранном диапазоне.");
+        }
+
+        private void Pd_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            if (currentPrintIndex >= studentsToPrint.Count)
+            {
+                e.HasMorePages = false;
+                return;
+            }
+
+            Student student = studentsToPrint[currentPrintIndex];
+            Log($"Печать карточки [{currentPrintIndex + 1}/{studentsToPrint.Count}]: {student.LastName} {student.FirstName} (Карта №{currentCardNumber})");
+
+            using (Bitmap cardImg = GenerateCard(student, currentCardNumber, templateImage))
+            {
+                e.Graphics.DrawImage(cardImg, e.PageBounds);
+            }
+
+            currentPrintIndex++;
+            currentCardNumber++;
+
+            e.HasMorePages = currentPrintIndex < studentsToPrint.Count;
+        }
+
+        private Bitmap GenerateCard(Student student, int cardNumber, Image template)
+        {
+            Bitmap bmp = new Bitmap(template.Width, template.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+
+                g.DrawImage(template, 0, 0, template.Width, template.Height);
+
+                if (student.Photo != null)
+                {
+                    Rectangle photoRect = new Rectangle(36, 96, 299, 391);
+                    g.DrawImage(student.Photo, photoRect);
+                }
+
+                using (Font fontBase = new Font("Segoe UI Black", 24, GraphicsUnit.Pixel))
+                using (Font fontNum = new Font("Segoe UI Black", 28, GraphicsUnit.Pixel))
+                {
+                    g.DrawString(student.LastName, fontBase, Brushes.Black, new Point(359, 157));
+                    g.DrawString(student.FirstName, fontBase, Brushes.Black, new Point(359, 233));
+                    g.DrawString(student.DateOfBirth, fontBase, Brushes.Black, new Point(359, 306));
+                    g.DrawString(student.Group, fontBase, Brushes.Black, new Point(359, 383));
+                    g.DrawString(student.Speciality, fontBase, Brushes.Black, new Point(359, 462));
+
+                    g.DrawString(student.Iin, fontBase, Brushes.Black, new Point(111, 530));
+                    g.DrawString(cardNumber.ToString(), fontNum, Brushes.Black, new Point(888, 127));
+                }
+            }
+            return bmp;
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label3_Click(object sender, EventArgs e) { }
+
+        private void btnPrint_Click_1(object sender, EventArgs e)
+        {
+            btnPrint_Click(sender, e);
+        }
+
+        private void numStart_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numEnd_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numCardStart_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtExcel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
